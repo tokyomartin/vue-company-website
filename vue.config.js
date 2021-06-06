341cf1d191cc4875de1afbd259475375ae86b058
kenny67 <282716545@qq.com>
2021-06-06 16:07:38 +0900

    lang

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
 
   publicPath: './',
-  
+
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
@@ -12,13 +12,19 @@ module.exports = {
       })
     ]
   },
+  devServer:{
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true,
+    disableHostCheck: true,
+  },
   pwa: {
     name: 'Vue Argon Design',
     themeColor: '#172b4d',
     msTileColor: '#172b4d',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: '#172b4d',
-    
+
   },
   css: {
     // Enable CSS source maps.
